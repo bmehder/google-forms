4147ac0c1a1b0242b651cc8f7efc742625ea3b92
bmehder <bmehder@gmail.com>
2021-12-23 15:07:29 +0100

    added Notes

---
+++ src/routes/notes.svelte
@@ -0,0 +1,37 @@
+<p>Not best to use right now if:</p>
+<ul>
+  <li>Need to process payments on form submission</li>
+  <li>Need to send an autoresponder email</li>
+  <li>
+    <ul>
+      <li>But maybe autoresponders can be done via Google Forms instead?</li>
+    </ul>
+  </li>
+</ul>
+
+<p>Best to use:</p>
+<ul>
+  <li>When conditional logic is needed in the form</li>
+  <li>
+    When clients need to see the records because they never really do it through
+    WP
+  </li>
+</ul>
+
+<p>Form plugins are 99% of the reason I've use WP.</p>
+<p>Formspree is good, but limited.</p>
+<p>A website stack should not be dictated by the form software.</p>
+<p>
+  Need to use a form plugin? Then it instantly requires paid hosting and makes
+  SEO way worse.
+</p>
+<p>
+  A WP site will cost 10 times as much in development costs. That goes up over
+  time.
+</p>
+
+<style>
+  ul {
+    list-style-type: none;
+  }
+</style>
